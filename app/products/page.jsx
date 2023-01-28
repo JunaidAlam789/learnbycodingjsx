79c940d4d705d8a5a59689574ba559bd0f63df88
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2023-01-28 19:19:56 +0500

    Catalog

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -10,10 +10,25 @@ export default function ProductPage(){
 const products= use(getProducts())
 return(
 <div>
+<h1>Product Catalog</h1>
+<div>
 {products.map(product=>
    <div key={product.name}>
      <Link  href={`/products/${product.name.replace(/\s+/g,"-").toLowerCase()}`}>{product.name}</Link>
     
+      </div>
+    )}
+</div>
+{products.map(product=>
+   <div key={product.name}>
+    {/*  <h1>{product.name}</h1> */}
+    <h1><Link  href={`/products/${product.name.replace(/\s+/g,"-").toLowerCase()}`}>{product.name}</Link></h1>
+     <Image alt="leeerob" src={product.image} width="500" height="333"/>
+     <h3>
+   {/*  {product.type} {' '} */}
+    ${product.price} {' '}
+    {product.isOnStock ? 'Available': 'Out of Stock'} </h3>
+    {/*  <h3>{product.image}</h3> */}
       </div>
     )}
 </div>

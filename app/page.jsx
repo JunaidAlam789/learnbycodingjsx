f7534f10ffe98c23aa44718e9a6cd43b263c2499
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2023-01-28 20:53:38 +0500

    product link in home
    product link in home

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,12 @@
 import Navigation from "./shared/Navbar"
+import Link from "next/link"
 export default function Page(){
 
 return(
     <>
-    <div className={"headerArea"}><Navigation/></div>
+    <div className={"headerArea"}>
+    <Link href={'/products'}>Products</Link>
+    </div>
 
     <div className={"container"}><h1>Welcome to Store!!!</h1></div>
     <div className={"footerArea"}>Footer Area !!!</div>

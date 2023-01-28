f57119be88777dbd1af520fe8b15009b2e72dc1d
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2023-01-28 19:56:46 +0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ return(
     <>
     <div className={"headerArea"}><Navigation/></div>
 
-    <div className={"container"}>Hello from Page!!!</div>
+    <div className={"container"}><h1>Welcome to Store!!!</h1></div>
     <div className={"footerArea"}>Footer Area !!!</div>
     </>
 )
